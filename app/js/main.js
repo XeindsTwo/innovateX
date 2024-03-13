33e6e6c9c9c0b0737c91b0091faf6f7336ee0e2a
XeindsTwo <125365638+XeindsTwo@users.noreply.github.com>
2024-03-13 05:37:32 +0300

    final

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,15 @@
+const body = document.body;
+const menuBtn = document.querySelector('.menu-btn');
+const nav = document.querySelector('.nav__mobile');
 const anchors = document.querySelectorAll('a[href*="#"]');
+
+menuBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  body.classList.toggle('body--active');
+  nav.classList.toggle('active');
+  menuBtn.classList.toggle('active');
+  menuBtn.blur();
+});
 
 function scrollToTarget(targetId) {
   const targetSection = document.querySelector(targetId);
@@ -15,10 +26,29 @@ function scrollToTarget(targetId) {
   }
 }
 
+function scrollToTargetMobile(targetId) {
+  const targetSection = document.querySelector(targetId);
+  if (targetSection) {
+    body.classList.remove('body--active');
+    nav.classList.remove('active');
+    menuBtn.classList.remove('active');
+    setTimeout(() => {
+      const targetOffset = targetSection.offsetTop;
+      window.scrollTo({top: targetOffset, behavior: 'smooth'});
+    }, 400);
+  }
+}
+
 function handleAnchorClick(event) {
   event.preventDefault();
   const targetId = this.getAttribute('href');
   scrollToTarget(targetId);
+}
+
+function handleAnchorClickMobile(event) {
+  event.preventDefault();
+  const targetId = this.getAttribute('href');
+  scrollToTargetMobile(targetId);
 }
 
 for (const anchor of anchors) {
@@ -26,10 +56,15 @@ for (const anchor of anchors) {
   anchor.addEventListener('touchstart', handleAnchorClick, {passive: true});
 }
 
-const menuLinks = document.querySelectorAll('.header__link');
+for (const anchor of anchors) {
+  anchor.addEventListener('click', handleAnchorClickMobile);
+  anchor.addEventListener('touchstart', handleAnchorClickMobile, {passive: true});
+}
+
+const menuLinks = document.querySelectorAll('.nav__link');
 menuLinks.forEach((menuLink) => {
-  menuLink.addEventListener('click', handleAnchorClick);
-  menuLink.addEventListener('touchstart', handleAnchorClick, {passive: true});
+  menuLink.addEventListener('click', handleAnchorClickMobile);
+  menuLink.addEventListener('touchstart', handleAnchorClickMobile, {passive: true});
 });
 
 const sections = document.querySelectorAll('section[id]');
